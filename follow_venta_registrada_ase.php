5ece88e3f9d15159115c5453acafe794a959b954
rodrigo cornejo <rodrigo_gfa@hotmail.com>
2023-11-24 18:54:14 -0500

    cambios

--- follow_venta_registrada_ase.php
+++ follow_venta_registrada_ase.php
@@ -14,14 +14,22 @@ $resultado = $conexion->query($sql);
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
+  <script>
+        $(document).ready( function () {
+            $('#ventas').DataTable();
+        } );
+    </script>
     <h1>SEGUIMIENTO DE VENTAS PENDIENTES</h1>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
-<div class="container-mt5">
+<div class="container">
 	<table class="table table-light" id="ventas">
 		<thead>
 			<tr class="table-dark">
--- registro_ventas.php
+++ registro_ventas.php
@@ -4,7 +4,6 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
@@ -110,7 +109,7 @@
 			<label for="observaciones">OBSERVACIONES </label>
 			<textarea name="observaciones" class="form-control" id="observaciones"></textarea>
 		</div>
-        
+
 		<input type="submit" value="enviar" class="boton1">
 	</form>
 </div>
